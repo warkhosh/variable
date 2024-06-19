cf3b8dd44b9b0ea1180a2407d7281b9ad51f753a
ekv.programmer <ekv.programmer@gmail.com>
2024-06-19 16:33:27 +0300

    refactor: some edit

--- VariableOptions.php
+++ VariableOptions.php
@@ -4,7 +4,7 @@ namespace Warkhosh\Variable;
 
 class VariableOptions
 {
-    const TRIM_REMOVE_CHAR = " \t\n\r\0\x0B";
-    const REMOVE_SPECIAL_SYMBOL = ["\n", "\r", "\t"];
-    const COST = 'cost';
+    public const TRIM_REMOVE_CHAR = " \t\n\r\0\x0B";
+    public const REMOVE_SPECIAL_SYMBOL = ["\n", "\r", "\t"];
+    public const COST = 'cost';
 }
